04086c0615d450b8d2d580ee851e092115cae0b6
KatyNunez <katiuskainf03@gmail.com>
2023-08-12 20:02:09 -0400

    average/sum

---
+++ accumulativesumloop.py
@@ -0,0 +1,10 @@
+
+# this program calculate the average and the total 
+total=0
+average=0
+count=0
+for valor in (10,2,3):
+    count+=1
+    total+=valor #acumulative variable
+print(" Suma de los numeros",total)
+print("promedio ",int(total/count))
